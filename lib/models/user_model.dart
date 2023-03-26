bc0c810c1705bf3ed4a44abbf8cbd4d769e36636
Abhiram K Pradeep <abhiramkpradeep123@gmail.com>
2023-03-27 01:11:00 +0530

    models

---
+++ lib/models/user_model.dart
@@ -0,0 +1,11 @@
+class UserModel {
+  final String user;
+  final String email;
+  final String password;
+
+  const UserModel({
+    required this.user,
+    required this.email,
+    required this.password,
+  });
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:ar_indoor_nav/pages/login.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
